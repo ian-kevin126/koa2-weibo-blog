bce80b2d80f41196188bfc74cb6a64ce36c19bd0
ian_kevin126 <ian_kevin126@126.com>
2021-08-16 00:11:51 +0800

    feat: add models

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
 
 const { isProd } = require('./utils/env')
+const { REDIS_CONF } = require('./conf/db')
 
 const index = require('./routes/index')
-const users = require('./routes/users')
+const userViewRouter = require('./routes/view/user')
 const errorViewRouter = require('./routes/view/error')
 
 // error handler
@@ -43,6 +46,25 @@ app.use(
   })
 )
 
+// 配置session
+app.keys = ['Uildsajda_8dsadsak&dasd_21%'] // 加密秘钥
+app.use(
+  session({
+    key: 'weibo.sid', // cookie name 默认是 `koa.sid`
+    prefix: 'weibo:sess:', // redis key 的前缀，默认是 `koa:sess:`
+    cookie: {
+      path: '/', // 生成的cookie在整个网站都是可用的
+      httpOnly: true, // 不允许客户端修改cookie
+      maxAge: 24 * 60 * 60 * 1000, // 过期时间，单位 ms
+    },
+    // 将session存到redis中
+    // ttl: 24 * 60 * 60 * 1000, // redis的过期时间，默认是和maxAge一样的，可不写。
+    store: redisStore({
+      all: `${REDIS_CONF.host}:${REDIS_CONF.port}`,
+    }),
+  })
+)
+
 // logger，跟上面的logger有点重复，是一个中间件演示，可以注释掉
 // app.use(async (ctx, next) => {
 //   const start = new Date()
@@ -53,8 +75,10 @@ app.use(
 
 // routes
 
-app.use(users.routes(), users.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// app.use(users.routes(), users.allowedMethods())
+// app.use(users.routes(), users.allowedMethods())
+app.use(userViewRouter.routes(), userViewRouter.allowedMethods())
+
 // 404的路由一定要注册到最后，只有当所有的路由没有命中才走404
 app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 // error-handling
